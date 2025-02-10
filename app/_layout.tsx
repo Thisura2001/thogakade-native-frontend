c0dea76ea96d0afe91bb9ec09f01fc2b091d6c0b
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-02-10 08:56:08 +0530

    Add layout for the project

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import {Stack} from "expo-router";
+
+function RootLayout() {
+    return(
+        <Stack>
+            <Stack.Screen name = 'index' options={{headerTitle:'Dashboard'}}/>
+            <Stack.Screen name = 'customer'/>
+            <Stack.Screen name = 'Item'/>
+        </Stack>
+    )
+}
+export default RootLayout;
